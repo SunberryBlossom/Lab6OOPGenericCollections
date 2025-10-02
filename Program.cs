9a486f938e98f8d4005e456d157ba14dbf2a2dca
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-10-02 20:27:35 +0200

    Added a foreach loop to print out all Employees in the Stack

--- Program.cs
+++ Program.cs
@@ -4,16 +4,23 @@
     {
         static void Main(string[] args)
         {
-            // Creating a Stack with five objects in
+            // Creating a Stack to contain Employee objects
             Stack<Employee> employeeStack = new Stack<Employee>();
 
             // Pushing in five new employees with filled in property values
             employeeStack.Push(new Employee { Id = "E001", Name = "Mika", Salary = 25600m, Gender = Gender.Female});
             employeeStack.Push(new Employee { Id = "E002", Name = "Elvira", Salary = 38400m, Gender = Gender.Female });
             employeeStack.Push(new Employee { Id = "E003", Name = "Andreas", Salary = 30000m, Gender = Gender.Male });
-            employeeStack.Push(new Employee { Id = "E004", Name = "Sam", Salary = 19800m, Gender = Gender.NonBinary });
+            employeeStack.Push(new Employee { Id = "E004", Name = "Sam", Salary = 19800m, Gender = Gender.Other });
             employeeStack.Push(new Employee { Id = "E005", Name = "Hera", Salary = 6000m, Gender = Gender.Female });
 
+
+            foreach (var employee in employeeStack)
+            {
+                Console.WriteLine($"Name: {employee.Name}\nWork ID: {employee.Id}\nCurrent Salary: {employee.Salary:C0}\nGender: {employee.Gender}");
+                Console.WriteLine($"Items left in the stack: {employeeStack.Count}");
+                Console.WriteLine("--------------------------");
+            }
 
         }
     }
